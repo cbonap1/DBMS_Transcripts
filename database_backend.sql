9fad7dfa8925efb8168c5d5bb00b2accea4f788b
kpersand2025 <125592549+kpersand2025@users.noreply.github.com>
2023-11-22 23:37:55 -0500

    Update database_backend.sql

--- database_backend.sql
+++ database_backend.sql
@@ -7,9 +7,9 @@ USE UNIVERSITY;
 --TABLE STUDENTS
 CREATE TABLE STUDENTS(
   studentid INT(11) NOT NULL,
-  lastname VARCHAR(80) NOT NULL,
-  firstname VARCHAR(80) NOT NULL,
-  dob date NULL,
+  lastname VARCHAR(45) NOT NULL,
+  firstname VARCHAR(45) NOT NULL,
+  dob DATE NULL,
   CONSTRAINT STUDENTS_PK PRIMARY KEY(studentid)
   );
   --Insert Values into Students
@@ -28,8 +28,8 @@ SELECT * FROM STUDENTS;
 --TABLE COURSES
   CREATE TABLE COURSES(
     courseid INT(11) NOT NULL,
-    coursename VARCHAR(40) NOT NULL,
-    credits DOUBLE NOT NULL,
+    coursename VARCHAR(45) NOT NULL,
+    credits VARCHAR(45) NOT NULL,
     CONSTRAINT COURSES_PK PRIMARY KEY(courseid)
     );
 --INSERT VALUES INTO COURSES
@@ -46,23 +46,23 @@ SELECT * FROM COURSES;
 
 --TABLE GRADES
     CREATE TABLE GRADES(
-      term VARCHAR(10) NOT NULL,
       courseid INT(11) NOT NULL,
       studentid INT(11) NOT NULL,
-      grade VARCHAR(2) NOT NULL,
+      term VARCHAR(45) NOT NULL,
+      grade VARCHAR(45) NOT NULL,
       CONSTRAINT GRADES_PK PRIMARY KEY(term, courseid, studentid),
       CONSTRAINT GRADES_COURSES_FK FOREIGN KEY(courseid) REFERENCES COURSES(courseid),
       CONSTRAINT GRADES_STUDENTS_FK FOREIGN KEY(studentid) REFERENCES STUDENTS(studentid)
       );
       --INSERT VALUES INTO GRADES
-      INSERT INTO GRADES(term, courseid, studentid, grade)
-      ->VALUES('Spring 2023', '0145443','01','A');
-      INSERT INTO GRADES(term, courseid, studentid, grade)
-      ->VALUES('Fall 2022','301001','02','D');
-      INSERT INTO GRADES(term, courseid, studentid, grade)
-      ->VALUES('Summer 2020','481001','03','C');
-      INSERT INTO GRADES(term, courseid, studentid, grade)
-      ->VALUES('Fall 2022', '225001','04','A');
+      INSERT INTO GRADES(courseid, studentid, term, grade)
+      ->VALUES('0145443','01','Spring 2023','A');
+      INSERT INTO GRADES(courseid, studentid, term, grade)
+      ->VALUES('301001','02','Fall 2022','D');
+      INSERT INTO GRADES(courseid, studentid, term, grade)
+      ->VALUES('481001','03','Summer 2020','C');
+      INSERT INTO GRADES(courseid, studentid, term, grade)
+      ->VALUES('225001','04','Fall 2022','A');
     --DISPLAY GRADES TABLE WITH VALUES
         SELECT * FROM GRADES;
       
